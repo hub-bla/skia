7fd152237d79a8a1ce0e079ed25806927e006cba
Hubert Blaszczyk <hubert.blaszczyk@jetbrains.com>
2026-03-26 11:26:33 +0100

    add missing install command in pip3 calls

--- tools/skia_release/prepare_linux_arm.sh
+++ tools/skia_release/prepare_linux_arm.sh
@@ -18,4 +18,4 @@ apt-get install python3.9 -y
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 100
 
 apt-get install -y python3-pip
-pip3 "cmake==3.30.*"
+pip3 install "cmake==3.30.*"
