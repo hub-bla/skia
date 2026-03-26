291162f0a261337e6ff8394da1d14fe6e4ae3250
Hubert Blaszczyk <hubert.blaszczyk@jetbrains.com>
2026-03-26 08:32:13 +0100

    add ninja to linux arm and wasm prep scripts

--- tools/skia_release/prepare_linux_wasm.sh
+++ tools/skia_release/prepare_linux_wasm.sh
@@ -6,7 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get install binutils build-essential -y
 apt-get install software-properties-common -y
-apt-get install git cmake curl wget -y
+apt-get install git cmake ninja-build curl wget -y
 
 apt-get install python3.9 -y
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 100
