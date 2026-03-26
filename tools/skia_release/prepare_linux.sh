56529c4e29ccc89432f2a771b103d8e1ce0f8a05
Hubert Blaszczyk <hubert.blaszczyk@jetbrains.com>
2026-03-26 10:50:55 +0100

    get default cmake for linux x64

--- tools/skia_release/prepare_linux.sh
+++ tools/skia_release/prepare_linux.sh
@@ -15,7 +15,7 @@ update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/b
 update-alternatives --set gcc /usr/bin/gcc-11
 
 apt-get install git wget -y
-apt-get install unzip ninja-build fontconfig libfontconfig1-dev libglu1-mesa-dev curl zip pkg-config -y
+apt-get install unzip cmake ninja-build fontconfig libfontconfig1-dev libglu1-mesa-dev curl zip pkg-config -y
 
 apt-get install python3.9 -y
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 100
