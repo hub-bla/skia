faf926b05b28b33daac28c36a77044a770a08887
Hubert Błaszczyk <hubertblaszczyk03@gmail.com>
2026-03-30 09:11:37 +0200

    remove unused wasm in dawn in cmake_utils

--- third_party/dawn/cmake_utils.py
+++ third_party/dawn/cmake_utils.py
@@ -361,11 +361,6 @@ def combine_into_library(args, output_path, build_dir, target_os, object_files):
     combine_obj_cmd = [
         linker_exe, "/LIB", f"/OUT:{lib_name}", f"@{response_file_name}"
     ]
-  elif target_os == "wasm":
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-    emsdk_env_path = os.path.abspath(os.path.join(script_dir, "..", "externals", "emsdk"))
-    emar = os.path.join(emsdk_env_path, "upstream", "emscripten", "emar")
-    combine_obj_cmd = [emar, "rcs", lib_name] + object_files
   else:
     combine_obj_cmd = ["ar", "rcs", lib_name] + object_files
   subprocess.run(combine_obj_cmd, cwd=build_dir, check=True)
