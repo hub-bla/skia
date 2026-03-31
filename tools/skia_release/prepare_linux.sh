4fbbee29901da4353da368825172276572751306
Hubert Blaszczyk <hubert.blaszczyk@jetbrains.com>
2026-03-31 10:33:41 +0200

    add cmake to linux bash scripts

--- tools/skia_release/prepare_linux.sh
+++ tools/skia_release/prepare_linux.sh
@@ -15,7 +15,10 @@ update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/b
 update-alternatives --config gcc
 
 apt-get install git wget -y
-apt-get install ninja-build fontconfig libfontconfig1-dev libglu1-mesa-dev curl zip -y
+apt-get install unzip fontconfig libfontconfig1-dev libglu1-mesa-dev curl zip pkg-config -y
 
 apt-get install python3.9 -y
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 100
+
+apt-get install -y python3-pip
+pip3 install "cmake==3.30.*" "ninja>=1.11.1"
--- tools/skia_release/prepare_linux_arm.sh
+++ tools/skia_release/prepare_linux_arm.sh
@@ -16,3 +16,6 @@ update-alternatives --config gcc
 
 apt-get install python3.9 -y
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 100
+
+apt-get install -y python3-pip
+pip3 install "cmake==3.30.*" "ninja>=1.11.1"
