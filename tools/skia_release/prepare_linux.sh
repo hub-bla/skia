3f7a489069af479156dd52f72a6b1013df7d1e3a
Hubert Blaszczyk <hubert.blaszczyk@jetbrains.com>
2026-04-14 18:12:53 +0200

    Revert linux preparation scripts changes

--- tools/skia_release/prepare_linux.sh
+++ tools/skia_release/prepare_linux.sh
@@ -8,27 +8,14 @@ apt-get install build-essential software-properties-common -y
 add-apt-repository ppa:ubuntu-toolchain-r/test -y
 apt-get update -y
 apt-get install build-essential software-properties-common -y
-apt-get install -y \
-  libx11-xcb-dev \
-  libxcb-dri2-0-dev \
-  libxcb-dri3-dev \
-  libxcb-present-dev \
-  libxcb-sync-dev \
-  libxcb-xfixes0-dev \
-  libxrandr-dev \
-  libxext-dev \
-  libxi-dev
 apt-get update
 
 apt-get install gcc-10 g++-10 -y
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
-update-alternatives --set gcc /usr/bin/gcc-10
+update-alternatives --config gcc
 
 apt-get install git wget -y
-apt-get install unzip fontconfig libfontconfig1-dev libglu1-mesa-dev curl zip pkg-config -y
+apt-get install ninja-build fontconfig libfontconfig1-dev libglu1-mesa-dev curl zip -y
 
 apt-get install python3.9 -y
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 100
-
-apt-get install -y python3-pip
-pip3 install "cmake==3.30.*" "ninja>=1.11.1"
--- tools/skia_release/prepare_linux_wasm.sh
+++ tools/skia_release/prepare_linux_wasm.sh
@@ -6,10 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get install binutils build-essential -y
 apt-get install software-properties-common -y
-apt-get install git unzip curl wget pkg-config -y
+apt-get install git curl wget -y
 
 apt-get install python3.9 -y
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 100
-
-apt-get install -y python3-pip
-pip3 install "cmake==3.30.*" "ninja>=1.11.1"
